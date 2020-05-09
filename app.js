69294cb30b4de30dcfeaa35a40bb86be9d238317
Eddie Waweru <wambuguedison@gmail.com>
2020-05-09 18:11:08 +0300

    optimize for deployment

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((res, req, next) => {
+  res.json({
+    "message": "atleast it runs for now"
+  });
+  console.log('atleast it runs');
+});
+
 module.exports = app;
