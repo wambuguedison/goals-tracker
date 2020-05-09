6aee5c58dcc5a98a236fc157f34d46bd31d5dc9a
Eddie Waweru <wambuguedison@gmail.com>
2020-05-09 18:26:48 +0300

    Fix CORS errors

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((res, req, next) => {
-  res.json({
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use("/", (res, req, next) => {
+  res.status(200).json({
     "message": "atleast it runs for now"
   });
   console.log('atleast it runs');
