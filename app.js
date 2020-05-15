17baa2dbd2a74f5d71dcac4af78d3d8c96a3e3a9
Eddie Waweru <wambuguedison@gmail.com>
2020-05-15 21:51:54 +0300

    optimize backend structure for easy maintenamce

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const goals = require('./models/db')
 
+const router = require('./routes/goals')
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -37,135 +39,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res, next) => {
-  goals.find({}, (err, docs) => {
-    if(err){
-      console.log(err)
-    }
-    let object = {
-      goals: docs,
-      template: 'home',
-      goals_present: false
-    }
-    if (docs.length > 0) {
-      object.goals_present = true;
-    }
-    res.render('index', object);
-  });
-});
-
-app.get('/view/:id', (req, res, next) => {
-  let id = req.params.id.replace(':', '');
-  goals.find({ _id: id }, (err, doc) => {
-    if(err) {
-      res.send(err)
-    }
-    res.send(doc)
-  });
-})
-
-app.get('/add', (req, res, next) => {
-  let add = {
-    add_new: 'added',
-    template: 'add'
-  }
-  res.render('add', add)
-});
-
-app.post('/add_goal', (req, res, next) => {
-  const goal = {
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: '',
-    successes: 0,
-    deleted: 0,
-    created_at: new Date().toDateString()
-  };
-  goals.insert(goal,(err, doc) => {
-  if(err){
-    console.error(err)
-  }
-  let added = {
-    title: doc.title,
-    description: doc.description,
-    id: doc._id,
-    add: 'added',
-    template: 'added'
-  }
-  res.render('add', added)
-})
-});
-
-app.get('/edit/:id', (req, res, next) => {
-  let id = req.params.id.replace(':', '');
-  goals.find({ _id: id }, (err, doc) => {
-    if(err) {
-      console.log(err)
-    }
-    let objects = {
-      goal: doc
-    }
-    res.render('update', objects)
-  });
-});
-
-app.post('/update', (req, res, next) => {
-  const goal = {
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: '',
-    successes: 0,
-    deleted: 0
-  };
-  goals.update({_id: req.body.id}, goal, {}, (err, numReplaced) => {
-    if(err){
-      res.status(400).json({
-        error: err
-      });
-    }
-    let update = {
-      title: goal.title,
-      update: 'updated',
-      template: 'edit'
-    }
-    res.render('update', update);
-  });
-});
-
-app.get('/delete/:id', (req, res, next) => {
-  let id = req.params.id.replace(':', '');
-  goals.find({ _id: id }, (err, doc) => {
-    if(err) {
-      console.log(err)
-    }
-    let delete_object = {
-      del_goal: doc,
-      template: 'delete'
-    }
-    res.render('update', delete_object);
-  });
-});
-
-
-app.get('/del_goal/:id', (req, res, next) => {
-  let id = req.params.id.replace(':', '');
-  goals.remove({ _id: id }, {}, (err, numRemoved) => {
-    let delete_goal = {
-      delete: 'deleted',
-      template: 'deleted'
-    }
-    res.render('update', delete_goal);
-  });
-});
-
-app.delete('/delete_all', (req, res, next) => {
-  goals.remove({}, { multi: true }, (err, numRemoved) => {
-    if(err) {
-      console.log(err)
-    }
-    res.send(String(numRemoved));
-  });
-});
+app.use('', router);
 
 app.use((req, res) => {
   res.render('error');
