97e15ef13077229775f76627619a2c9271e4fed1
Eddie Waweru <wambuguedison@gmail.com>
2020-05-18 14:30:07 +0300

    basic signup function without hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,30 @@
+const db = require('../models/db');
+const users = db.users
+
 exports.signup = (req, res, next) => {
-
+  const user = {}
+  let email = req.body.email,
+    password = req.body.password;
+  
+  users.findOne({ email: email }, function (err, doc) {
+    if (err) {
+      res.send(err)
+    }
+    if (doc !== null) {
+      res.send("email is already used")
+    } else {
+      user.email = email;
+      user.password = password;
+      users.insert(user, (err, doc) => {
+        if(err){
+          res.send(err)
+        }
+        res.send(doc)
+      })
+    }
+  });
 };
 
-exports.login = (req, res, next) => {
+/*exports.login = (req, res, next) => {
 
-};
+};*/
