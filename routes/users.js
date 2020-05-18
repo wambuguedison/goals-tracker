3705f6ad150139cbda133e8c560adbdf930b0344
Eddie Waweru <wambuguedison@gmail.com>
2020-05-18 14:27:34 +0300

    create user signup route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const userControllers = require('../controllers/user');
+const userControllers = require('../controllers/users');
 
 router.post('/signup', userControllers.signup);
-router.post('/login', userControllers.login);
+//router.post('/login', userControllers.login);
 
 module.exports = router;
