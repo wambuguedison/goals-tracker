d5ec8fc329772f746dfc67b1880dc223cb0a4896
Eddie Waweru <wambuguedison@gmail.com>
2020-05-20 15:50:03 +0300

    add signup and login routes

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const userControllers = require('../controllers/users');
 
 router.post('/signup', userControllers.signup);
 router.post('/login', userControllers.login);
+router.get('/signup_form', userControllers.signup_form)
+router.get('/login_form', userControllers.login_form)
 router.get('/logout', userControllers.logout);
 
 module.exports = router;
