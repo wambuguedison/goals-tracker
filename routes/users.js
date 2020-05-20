b17c48b1978c556411922ef2f5cdef9afd05ed72
Eddie Waweru <wambuguedison@gmail.com>
2020-05-20 14:39:36 +0300

    Add logout route

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const userControllers = require('../controllers/users');
 
 router.post('/signup', userControllers.signup);
 router.post('/login', userControllers.login);
+router.get('/logout', userControllers.logout);
 
 module.exports = router;
