694761c87d133e74cac83f812cf8ddb11f4cda6c
Eddie Waweru <wambuguedison@gmail.com>
2020-05-20 15:44:53 +0300

    Render login page is token isn't found

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,15 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.cookies['AuthToken'];
-  if(!token || token == null || token == undefined) {
-    res.status(401).send('please log in')
-  }
-  const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-  const userId = decodedToken.userId;
-  if (req.body.userId && req.body.userId !== userId) {
-    res.status(401).send('Invalid user ID');
+  if(token) {
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      res.status(401).send('Invalid user ID');
+    } else {
+      next();
+    }
   } else {
-    next();
+    res.render('login');
   }
 };
