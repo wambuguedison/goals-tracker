3238f816138b47f6d5d4a40f36e1a289c02d6b46
Eddie Waweru <wambuguedison@gmail.com>
2020-05-20 14:36:54 +0300

    handle jwt error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.cookies['AuthToken'];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+  const token = req.cookies['AuthToken'];
+  if(!token || token == null || token == undefined) {
+    res.status(401).send('please log in')
+  }
+  const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+  const userId = decodedToken.userId;
+  if (req.body.userId && req.body.userId !== userId) {
+    res.status(401).send('Invalid user ID');
+  } else {
+    next();
   }
 };
